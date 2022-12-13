55cb91210e54dd59cf3c49d93bb0b4dbffdcdb65
Yoo Yunho <yyh3690@naver.com>
2022-12-13 17:14:11 +0900

    instagram app register

--- backend/instagram/admin.py
+++ backend/instagram/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from instagram.models import Tag, Comment, Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
