b8723c082b45b0566f1719e1f1f98767bd147df3
Yoo Yunho <yyh3690@naver.com>
2022-12-13 16:41:02 +0900

    instagram register

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -20,12 +20,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third apps
+
+    # local apps
+    'instagram',
 ]
 
 MIDDLEWARE = [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,9 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('', include('instagram.urls')),
+
 
 ]
 
