097b815d05ce862fe7b025ddf3edebe0ba72e84b
Yoo Yunho <yyh3690@naver.com>
2022-12-13 17:43:24 +0900

    DRF install

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third apps
-
+    'rest_framework',
     # local apps
     'instagram',
     'accounts',
@@ -120,3 +120,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path('', include('instagram.urls')),
-
+    path('api-auth/', include('rest_framework.urls'))
 
 ]
 
